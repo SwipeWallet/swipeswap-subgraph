2d7fb303bac69b6805766cdddf5496c48b922cc5
techstreamp <techstream.pro@gmail.com>
2021-03-10 08:46:58 +1000

    Rename masterchef to swipeswap

--- src/lockup.ts
+++ src/lockup.ts
@@ -1,15 +1,15 @@
 import { Address, BigInt, ethereum, log } from '@graphprotocol/graph-ts'
-import { BIG_DECIMAL_1E12, BIG_DECIMAL_1E18, BIG_DECIMAL_ZERO, BIG_INT_ONE, BIG_INT_ZERO, LOCKUP_BLOCK_NUMBER, LOCKUP_POOL_NUMBER, MASTER_CHEF_ADDRESS } from './constants'
+import { BIG_DECIMAL_1E12, BIG_DECIMAL_1E18, BIG_DECIMAL_ZERO, BIG_INT_ONE, BIG_INT_ZERO, LOCKUP_BLOCK_NUMBER, LOCKUP_POOL_NUMBER, SWIPE_SWAP_ADDRESS } from './constants'
 import {
   Deposit,
-  MasterChef as MasterChefContract,
-  MasterChef__poolInfoResult,
+  SwipeSwap as SwipeSwapContract,
+  SwipeSwap__poolInfoResult,
   SetCall,
   Withdraw,
-} from '../generated/MasterChef/MasterChef'
+} from '../generated/SwipeSwap/SwipeSwap'
 import { Lockup, Pool, User } from '../generated/schema'
 import { getSwipePrice } from './price'
-import { Pair as PairContract } from '../generated/MasterChef/Pair'
+import { Pair as PairContract } from '../generated/SwipeSwap/Pair'
 
 export function getUser(pid: BigInt, address: Address, block: ethereum.Block): User {
   const uid = address.toHex()
@@ -36,11 +36,11 @@ export function getPool(id: BigInt): Pool {
   let pool = Pool.load(id.toString())
 
   if (pool === null) {
-    const masterChefContract = MasterChefContract.bind(MASTER_CHEF_ADDRESS)
+    const swipeSwapContract = SwipeSwapContract.bind(SWIPE_SWAP_ADDRESS)
 
     // Create new pool.
     pool = new Pool(id.toString())
-    const poolInfo = masterChefContract.poolInfo(id)
+    const poolInfo = swipeSwapContract.poolInfo(id)
     pool.allocPoint = poolInfo.value1
     pool.accSwipePerShare = poolInfo.value3
 
@@ -55,12 +55,12 @@ export function set(call: SetCall): void {
   if (call.inputs._pid == LOCKUP_POOL_NUMBER) {
     log.info('Alright stop, lockup time...', [])
 
-    const masterChefContract = MasterChefContract.bind(MASTER_CHEF_ADDRESS)
-    const poolLength = masterChefContract.poolLength()
+    const swipeSwapContract = SwipeSwapContract.bind(SWIPE_SWAP_ADDRESS)
+    const poolLength = swipeSwapContract.poolLength()
 
     const lockup = new Lockup('0')
     lockup.poolLength = poolLength
-    lockup.totalAllocPoint = masterChefContract.totalAllocPoint()
+    lockup.totalAllocPoint = swipeSwapContract.totalAllocPoint()
     lockup.save()
 
     log.info('Saved lockup entity, before loop. Pool length: {}', [poolLength.toString()])
@@ -68,9 +68,9 @@ export function set(call: SetCall): void {
     for (let i = BIG_INT_ZERO, j = poolLength; i < j; i = i.plus(BIG_INT_ONE)) {
       log.warning('Setting pool state at lockup for pid {}', [i.toString()])
 
-      let poolInfoResult: ethereum.CallResult<MasterChef__poolInfoResult> = masterChefContract.try_poolInfo(i)
+      let poolInfoResult: ethereum.CallResult<SwipeSwap__poolInfoResult> = swipeSwapContract.try_poolInfo(i)
 
-      let poolInfo: MasterChef__poolInfoResult = null
+      let poolInfo: SwipeSwap__poolInfoResult = null
 
       if (!poolInfoResult.reverted) {
         poolInfo = poolInfoResult.value
@@ -83,17 +83,17 @@ export function set(call: SetCall): void {
       pool.lockup = lockup.id
       pool.allocPoint = poolInfo.value1
       pool.accSwipePerShare = poolInfo.value3
-      // pool.balance = pairContract.balanceOf(MASTER_CHEF_ADDRESS)
+      // pool.balance = pairContract.balanceOf(SWIPE_SWAP_ADDRESS)
       pool.save()
     }
   }
 }
 
 function transfer(pid: BigInt, userAddr: Address, block: ethereum.Block): void {
-  const masterChefContract = MasterChefContract.bind(MASTER_CHEF_ADDRESS)
+  const swipeSwapContract = SwipeSwapContract.bind(SWIPE_SWAP_ADDRESS)
   const user = getUser(pid, userAddr, block)
 
-  const poolInfo = masterChefContract.poolInfo(pid)
+  const poolInfo = swipeSwapContract.poolInfo(pid)
   const pool = getPool(pid)
   pool.accSwipePerShare = poolInfo.value3
   pool.save()
@@ -112,7 +112,7 @@ function transfer(pid: BigInt, userAddr: Address, block: ethereum.Block): void {
       user.swipeHarvestedSinceLockupUSD = user.swipeHarvestedSinceLockupUSD.plus(swipeHarvestedUSD)
     }
   }
-  const userInfo = masterChefContract.userInfo(pid, userAddr)
+  const userInfo = swipeSwapContract.userInfo(pid, userAddr)
   user.amount = userInfo.value0
   user.rewardDebt = userInfo.value1
   user.save()
